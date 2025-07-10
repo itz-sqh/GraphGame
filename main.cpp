88df00a7568c750b6bd337ad3dffbd9238bf49f1
Rekname <malyshko.aleksandr.05@gmail.com>
2025-07-10 20:19:59 +0300

    first commit

--- main.cpp
+++ main.cpp
@@ -1,3 +1,20 @@
-int main() {
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(sf::Vector2u(200, 200)), "SFML Works!");
+    while (window.isOpen())
+    {
+        while (auto event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+        }
+        window.clear();
+        window.display();
+    }
+
     return 0;
 }
