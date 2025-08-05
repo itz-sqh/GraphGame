d2300fb2b760d805cfeb92eb61d506861e86426f
itz-sqh <itz.sqh@gmail.com>
2025-08-05 13:38:36 +0300

    Move InputBox method to .cpp instead of .h

--- include/ui/InputBox.h
+++ include/ui/InputBox.h
@@ -8,11 +8,11 @@
 class InputBox {
 public:
     InputBox();
-    void draw(sf::RenderTarget& target, const std::string& text, sf::Color outlineColor);
-    void drawInactive(sf::RenderTarget& target, const std::string& text) {
-        rect.setFillColor(sf::Color(200, 200, 200, 150));
-        draw(target, text, sf::Color(100, 100, 100));
-    }
+
+    void draw(sf::RenderTarget &target, const std::string &text, sf::Color outlineColor);
+
+    void drawInactive(sf::RenderTarget &target, const std::string &text);
+
 private:
     sf::RectangleShape rect;
     sf::Font font;
--- src/ui/InputBox.cpp
+++ src/ui/InputBox.cpp
@@ -2,13 +2,18 @@
 
 
 InputBox::InputBox() {
-    rect.setSize({400,40});
-    rect.setFillColor(sf::Color(255,255,255,150));
-    rect.setPosition({(GameConstants::WIDTH - 400.f) /2,GameConstants::HEIGHT-40.f});
+    rect.setSize({400, 40});
+    rect.setFillColor(sf::Color(255, 255, 255, 150));
+    rect.setPosition({(GameConstants::WIDTH - 400.f) / 2, GameConstants::HEIGHT - 40.f});
 
     if (!font.openFromFile("resources/ARIAL.TTF")) {
         //TODO handle error
     }
+}
+
+void InputBox::drawInactive(sf::RenderTarget &target, const std::string &text) {
+    rect.setFillColor(sf::Color(200, 200, 200, 150));
+    draw(target, text, sf::Color(100, 100, 100));
 }
 
 void InputBox::draw(sf::RenderTarget &target, const std::string &text, sf::Color outlineColor) {
@@ -21,14 +26,13 @@ void InputBox::draw(sf::RenderTarget &target, const std::string &text, sf::Color
     textDisplay.setFillColor(sf::Color::Black);
 
     while (textDisplay.getGlobalBounds().size.x - rect.getSize().x >= -2)
-        textDisplay.setCharacterSize(textDisplay.getCharacterSize()-1);
+        textDisplay.setCharacterSize(textDisplay.getCharacterSize() - 1);
 
     textDisplay.setOrigin(textDisplay.getGlobalBounds().size / 2.f + textDisplay.getLocalBounds().position);
-    textDisplay.setPosition(rect.getPosition() + rect.getSize()/2.f);
+    textDisplay.setPosition(rect.getPosition() + rect.getSize() / 2.f);
 
     target.draw(textDisplay);
 
 
     target.draw(textDisplay);
 }
-
